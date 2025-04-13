4493f729556f2a12036d21545135971e9319276e
Trekeptor <30782814+trekeptor@users.noreply.github.com>
2025-04-14 00:28:27 +0530

    deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,1 +1,19 @@
+const hre = require("hardhat");
+
+async function main() {
+  const Verifier = await hre.ethers.getContractFactory("Verifier");
+  const verifier = await Verifier.deploy();
+  await verifier.deployed();
+  console.log("Verifier deployed to:", verifier.address);
+
+  const Mixer = await hre.ethers.getContractFactory("Mixer");
+  const mixer = await Mixer.deploy(verifier.address);
+  await mixer.deployed();
+  console.log("Mixer deployed to:", mixer.address);
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
 
