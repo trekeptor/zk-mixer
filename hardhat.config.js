ac500f713949ce7ac0894a0be7a3895c7ed4c4b2
Trekeptor <30782814+trekeptor@users.noreply.github.com>
2025-04-14 00:27:55 +0530

    hardhat.config.js

---
+++ hardhat.config.js
@@ -0,0 +1,13 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+
+module.exports = {
+  solidity: "0.8.18",
+  networks: {
+    hardhat: {},
+    sepolia: {
+      url: process.env.SEPOLIA_RPC_URL,
+      accounts: [process.env.PRIVATE_KEY]
+    }
+  }
+};
